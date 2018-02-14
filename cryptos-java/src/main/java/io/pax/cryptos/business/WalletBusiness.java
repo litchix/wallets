17c2101673f91ccd5171f380be23565f438eaad0
litchix <alix.corneloup@gmail.com>
2018-02-14 16:27:02 +0100

    JEE WalletBusiness++++

--- cryptos-java/src/main/java/io/pax/cryptos/business/WalletBusiness.java
+++ cryptos-java/src/main/java/io/pax/cryptos/business/WalletBusiness.java
@@ -1,11 +1,13 @@
 package io.pax.cryptos.business;
 
 import io.pax.cryptos.domain.Wallet;
+import io.pax.cryptos.domain.jpa.JpaLine;
 import io.pax.cryptos.domain.jpa.JpaWallet;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 /**
  * It is a Java EE Managed EJB
@@ -20,7 +22,18 @@ public class WalletBusiness {
 
     public Wallet findWallet(int id) {
         //transaction is opened in your back
-        return em.find(JpaWallet.class, id);
+        JpaWallet w = em.find(JpaWallet.class, id);
+
+        String jpql = "SELECT l FROM JpaLine l JOIN l.wallet w WHERE w.id = :id"; //recupere le param "id"
+
+        List<JpaLine> lines = em.createQuery(jpql, JpaLine.class)
+                .setParameter("id", id)
+                .getResultList();
+
+        System.out.println(lines);
+
+        w.setLines(lines);
+        return w;
     }//and now closed.
 
 
